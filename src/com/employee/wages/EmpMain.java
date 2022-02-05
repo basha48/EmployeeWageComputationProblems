aecb56dc9ca2d165670d81cf92a028674f7c79b4
basha <bashamb787@gmail.com>
2022-02-05 14:30:47 +0530

    employee wage problems

--- src/com/employee/wages/EmpMain.java
+++ src/com/employee/wages/EmpMain.java
@@ -3,8 +3,7 @@ package com.employee.wages;
 public class EmpMain {
 	public static void main(String [] args) {
 	EmployeeWages emp = new EmployeeWages();
-	EmployeeWages emp1 = new EmployeeWages();
-	emp1.maxHrsReached();
+	emp.maxHrsReached();
 	}
 }
 
